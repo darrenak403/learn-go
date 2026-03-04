3192f920f8955daf7f4791e509d2e4fd3504d817
darrenak403 <ngothanhdat4002@gmail.com>
2026-03-04 10:43:37 +0700

    feat(pointer): add pointer demonstration in main.go
    Changes:
    - Create go.mod file for module definition
    - Implement pointer usage and demonstration in main.go
    - Add updateName function to illustrate variable scope and address

---
+++ lesson7-pointer/main.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+func lythuyetPointer() {
+	name := "Thanh Dat"
+	fmt.Println("-=-=-=-=-=-=-= Infomation name variable -=-=-=-=-=")
+	fmt.Printf("Data type: %T \n", name)
+	fmt.Printf("Value: %v \n", name)
+	fmt.Printf("Address: %v \n", &name)
+
+	//Tao con tro
+	ptrName := &name
+	fmt.Println("-=-=-=-=-=-=-= Infomation ptrName variable -=-=-=-=-=")
+	fmt.Printf("Data type: %T \n", ptrName)
+	fmt.Printf("Value: %v \n", ptrName)
+	fmt.Printf("Address: %v \n", &ptrName)
+
+	fmt.Printf("Find value name of ptrName: %v", *ptrName)
+
+	fmt.Println("-=-=-=-=-=-=-= Infomation ptrName2 variable -=-=-=-=-=")
+	//Tao con tro moi
+	ptrName2 := &ptrName
+	fmt.Printf("Data type: %T \n", ptrName2)
+	fmt.Printf("Value: %v \n", ptrName2)
+	fmt.Printf("Address: %v \n", &ptrName2)
+
+	fmt.Printf("Find value name of ptrName2: %v", **ptrName2)
+}
+
+func updateName(name string) {
+	name = "Updated Name"
+	fmt.Println("-=-=-=-=-=-=-= Infomation name variable in updateName function -=-=-=-=-=")
+	fmt.Printf("Data type: %T \n", name)
+	fmt.Printf("Value: %v \n", name)
+	fmt.Printf("Address: %v \n", &name)
+}
+
+func main() {
+	name := "Thanh Dat"
+	fmt.Println("-=-=-=-=-=-=-= Infomation name variable -=-=-=-=-=")
+	fmt.Printf("Data type: %T \n", name)
+	fmt.Printf("Value: %v \n", name)
+	fmt.Printf("Address: %v \n", &name)
+
+	name = "Thien Kieu"
+	fmt.Println("-=-=-=-=-=-=-= Infomation name variable afterUpdate -=-=-=-=-=")
+	fmt.Printf("Data type: %T \n", name)
+	fmt.Printf("Value: %v \n", name)
+	fmt.Printf("Address: %v \n", &name)
+
+	fmt.Println()
+
+	updateName(name)
+}
